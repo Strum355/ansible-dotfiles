9df0c18b20a326da4b0ba54fc158b2e50affd4d9
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-03-18 20:23:54 +0000

    Added git, kitty and fish settings

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+set fish_greeting ""
+set -x GOPATH $HOME/Go
+set -x PATH /usr/local/go/bin $HOME/.bin $GOPATH/bin $PATH
+set -x M2_HOME /opt/maven
+set -x M2 $M2_HOME/bin
+set -x PATH $M2 $PATH
+set -x RUST_BACKTRACE 1 
