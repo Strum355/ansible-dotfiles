dc5c8f5173c91c67d75d6e58e4b1af7339bcafc6
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-07-08 20:21:13 +0100

    Added bobthefish theme and settings

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,10 @@ set -x PATH /usr/local/go/bin $HOME/.bin $GOPATH/bin $PATH
 set -x M2_HOME /opt/maven
 set -x M2 $M2_HOME/bin
 set -x PATH $M2 $PATH
-set -x RUST_BACKTRACE 1 
+set -x RUST_BACKTRACE 1
+set -x GO111MODULE auto
+
+set -g theme_title_display_process yes
+set -g theme_title_use_abbreviated_path no
+set -g theme_show_exit_status yes
+set -g theme_color_scheme terminal
